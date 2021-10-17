904bec1841c13e5992dc446298f64b4f8c0f8c00
Livio Ara <assislivio@gmail.coom>
2021-10-17 15:13:06 -0300

    6

--- src/main/java/com/livio/cursomc/services/CategoriaService.java
+++ src/main/java/com/livio/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.livio.cursomc.domain.Categoria;
 import com.livio.cursomc.repositories.CategoriaRepository;
+import com.livio.cursomc.services.exceptions.ObjectNotFoundException;
+
+import net.bytebuddy.asm.Advice.Return;
 
 @Service
 public class CategoriaService {
@@ -16,10 +19,12 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 		
 	}
 
+	
 }
 
