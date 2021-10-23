bd92cb9425b83e4ad9e7fc142060d60752babd0b
Livio Ara <assislivio@gmail.coom>
2021-10-23 16:59:04 -0300

    PUT

--- src/main/java/com/livio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/livio/cursomc/resources/CategoriaResource.java
@@ -25,8 +25,8 @@ public class CategoriaResource {
 	
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer  id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer  id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
 	}
@@ -39,5 +39,16 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 		
 	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update (@RequestBody Categoria obj,@PathVariable Integer  id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+		
+	}
 }
+
+
 
