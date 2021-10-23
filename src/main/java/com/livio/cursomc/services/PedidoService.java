18270f7843179773bff7e2c274e3a311e2e3e2e4
Livio Ara <assislivio@gmail.coom>
2021-10-23 14:22:18 -0300

    11

---
+++ src/main/java/com/livio/cursomc/services/PedidoService.java
@@ -0,0 +1,28 @@
+package com.livio.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.livio.cursomc.domain.Pedido;
+import com.livio.cursomc.repositories.PedidoRepository;
+import com.livio.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+		
+	}
+
+	
+}
+
